f9fee200c0acdfd92039d5474c73118a7e6814ee
Archishman-Naskar <archishmannskr2005@gmail.com>
2025-10-20 02:45:02 +0530

    Integrating HTML With Flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
@@ -9,6 +9,14 @@ def welcome():
 @app.route("/demo")
 def demo():
   return "Welcome To Learning Flask.Creating Routes."
+
+@app.route("/demo/html1")
+def html1():
+  return "<html><h1>To Intregrate HTML Just Return HTML Text</h1><br><h2>Like This.</h2></html>"
+
+@app.route("/demo/html2")
+def html2():
+  return render_template("index.html")
 
 if __name__=="__main__":
   app.run(debug=True)
