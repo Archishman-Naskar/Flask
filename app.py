708351c5bcfd512e57e941185bbfdd5e001dca4f
Archishman-Naskar <archishmannskr2005@gmail.com>
2025-10-20 02:22:18 +0530

    Creating Route

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask
 
 app=Flask(__name__)
 
+@app.route("/")
+def welcome():
+  return "Welcome To Learning Flask."
+
+@app.route("/demo")
+def demo():
+  return "Welcome To Learning Flask.Creating Routes."
+
 if __name__=="__main__":
   app.run(debug=True)
 
