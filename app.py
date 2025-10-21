44202dec46040dfc9d6e64805e17e1868d7cc5b1
Archishman-Naskar <archishmannskr2005@gmail.com>
2025-10-21 19:36:08 +0530

    Using Jinja Tamplate to Integrate Python Code in HTML and Using the Data What We Got Form The FrontEnd or the BackEnd.

--- app.py
+++ app.py
@@ -27,6 +27,31 @@ def method():
     return f'Hello {name}'
   return render_template('form.html')
 
+# how can you use the data what you get from backend or frontend and use it
+# we use jinja 
+# no need to import or install any thing
+# it works just like that
+@app.route("/demo/<int:score>")
+def jinja(score):
+  return f'Score : {score}'
+
+# how to render for loop
+@app.route("/demo/integarteCodeInHTML/for/<int:score>")
+def jinjaFor(score):
+  status=""
+  if score>55 :
+    status="PASS"
+  else :
+    status="FAIL"
+  arg={'Status':status,'Score':score}
+  return render_template("forLoop.html",arg=arg)
+
+# how to render if
+@app.route("/demo/integarteCodeInHTML/if/<int:score>")
+def jinjaif(score):
+  
+  return render_template("if.html",score=score)
+
 if __name__=="__main__":
   app.run(debug=True)
 
